b0e89ddfccf2714abc93c710ba93a8f2d8c87d7b
Enkhai <aerogtx@gmail.com>
2021-04-21 01:39:25 +0300

    Improved script 1 and filled in on script 2. Script 2 is unfinished.

--- bitcoin_script_1.py
+++ bitcoin_script_1.py
@@ -5,35 +5,45 @@ from bitcoinutils.constants import TYPE_RELATIVE_TIMELOCK
 from bitcoinutils.keys import PrivateKey
 from bitcoinutils.script import Script
 from bitcoinutils.keys import P2shAddress
+from bitcoinrpc.authproxy import JSONRPCException
 
 
-def create_p2sh(proxy, wallet_file, address_name, blocks_to_lock):
+def create_p2sh(proxy, wallet_file, blocks_to_lock, address_name=None):
     """
-    Creates a P2SH address with a relative block timelock
-    Arguments
-        proxy: JSON RPC proxy for connecting to the network
-        wallet_file: File containing the wallet
-        address_name: Name of the address locking the funds
-        blocks_to_lock: Number of blocks the lock is valid for
-    Returns:
-        address_privk: Private key of the locking address
-        p2sh_addr: Private key of the created P2SH address
+    Creates a P2SH address with a relative block locktime
+
+        :param proxy: JSON RPC proxy for connecting to the network
+        :param wallet_file: File containing the wallet
+        :param blocks_to_lock: Number of future blocks the lock is valid for
+        :param address_name: Name of the address locking the funds. If None, a new address will be created
+        :return: tuple(address_privk, p2sh_addr): Tuple of private key of the locking address and key of the created P2SH address
     """
 
     # load wallet
-    proxy.loadwallet(wallet_file)
+    try:
+        proxy.loadwallet(wallet_file)
+    except JSONRPCException:
+        # wallet already loaded
+        pass
+
     # retrieve the public and private key strings of the specified address
-    address_pubk = list(proxy.getaddressesbylabel(address_name).keys())[0]
+    if address_name:
+        address_pubk = list(proxy.getaddressesbylabel(address_name).keys())[0]
+    else:
+        address_pubk = proxy.getnewaddress()
     address_privk = proxy.dumpprivkey(address_pubk)
-    # create key objects for the specified address
+
+    # create the P2PKH key object
     p2pkh_sk = PrivateKey(address_privk)
     p2pkh_pk = p2pkh_sk.get_public_key().get_address()
+
     # create sequence for the redeem script
     seq = Sequence(TYPE_RELATIVE_TIMELOCK, blocks_to_lock)
     # create the redeem script
     redeem_script = Script([seq.for_script(), 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP',
                             'OP_DUP', 'OP_HASH160', p2pkh_pk.to_hash160(),
                             'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+
     # create the P2SH address from the redeem script
     p2sh_addr = P2shAddress.from_script(redeem_script)
     # insert the P2SH address into the wallet
@@ -51,7 +61,7 @@ if __name__ == '__main__':
     # set up a JSON RPC proxy
     rpc_proxy = NodeProxy(username, password).get_proxy()
     # retrieve the private key of the locking address and the key of the created P2SH address
-    priv_key, p2sh_addr_key = create_p2sh(rpc_proxy, wallet, "address1", 10)
+    priv_key, p2sh_addr_key = create_p2sh(rpc_proxy, wallet, 10, "address1")
 
     # display the P2SH address key
     print("Created P2SH address key:", p2sh_addr_key)
--- bitcoin_script_2.py
+++ bitcoin_script_2.py
@@ -1,10 +1,15 @@
 from bitcoinutils.setup import setup
 from bitcoinutils.proxy import NodeProxy
+from bitcoinutils.transactions import Sequence, TxInput, TxOutput, Transaction
+from bitcoinutils.constants import TYPE_RELATIVE_TIMELOCK
+from bitcoinutils.keys import PrivateKey
+from bitcoinutils.script import Script
+import requests
+from decimal import Decimal
 
 from bitcoin_script_1 import create_p2sh
 
 if __name__ == '__main__':
-
     # set up the environment for regtest
     setup('regtest')
     username, password = "user", "pass"
@@ -12,7 +17,69 @@ if __name__ == '__main__':
     # set up a JSON RPC proxy
     rpc_proxy = NodeProxy(username, password).get_proxy()
 
-    # retrieve the private key of the locking address and the key of the created P2SH address
-    p2pkh_addr_priv_key, p2sh_addr_key = create_p2sh(rpc_proxy, "/home/will/.bitcoin/regtest/wallets/mywallet",
-                                   "address1", 10)
+    # retrieve the key of the created P2SH address
+    _, p2sh_addr_key = create_p2sh(rpc_proxy, "/home/will/.bitcoin/regtest/wallets/mywallet",
+                                   10)
+    # mine 100 blocks and send bitcoin to the P2SH address
+    rpc_proxy.generatetoaddress(100, p2sh_addr_key)
+    # mine 100 blocks to make mined bitcoin spendable, emulates 'bitcoin-cli -generate'
+    for _ in range(100):
+        rpc_proxy.generatetoaddress(1, rpc_proxy.getnewaddress())
 
+    # retrieve unspent UTXOs for the P2SH address
+    p2sh_addr_unspent = rpc_proxy.listunspent(0, 99999999, [p2sh_addr_key])
+
+    # create relative-block locking sequence for the transaction inputs
+    seq = Sequence(TYPE_RELATIVE_TIMELOCK, 10)
+
+    # create transaction inputs for unspent UTXOs
+    # and calculate the total unspent bitcoins they contain
+    tx_inputs = []
+    total_unspent = 0
+    for i in p2sh_addr_unspent:
+        tx_inputs.append(TxInput(i['txid'], i['vout'], sequence=seq.for_input_sequence()))
+        total_unspent += i['amount']
+    print("Unspent bitcoin in address {} : {}".format(p2sh_addr_key, total_unspent))
+
+    # create the address for the funds to be sent to
+    p2pkh_pub_addr = rpc_proxy.getnewaddress()
+    p2pkh_priv_addr = rpc_proxy.dumpprivkey(p2pkh_pub_addr)
+
+    p2pkh_sk = PrivateKey(p2pkh_priv_addr)
+    p2pkh_pk = p2pkh_sk.get_public_key()
+
+    # calculate fee to ensure transaction confirmation within half an hour
+    satoshis_per_kb = requests \
+        .get('https://bitcoinfees.earn.com/api/v1/fees/recommended') \
+        .json()['halfHourFee']
+    tx_size = len(tx_inputs) * 180 + 34 * 1 + 10 + len(tx_inputs)
+    fee = (tx_size / 1024) * (satoshis_per_kb / 10e+8)
+
+    # create the transaction output
+    tx_output = TxOutput(int(Decimal(total_unspent) - Decimal(fee) * Decimal(10e+8)),
+                         p2pkh_pk.get_address().to_script_pub_key())
+    # create the transaction
+    tx = Transaction(tx_inputs, [tx_output])
+    print('Raw unsigned transaction:', tx.serialize())
+
+    # create the redeem script
+    redeem_script = Script([seq.for_script(), 'OP_CHECKLOCKTIMEVERIFY', 'OP_DROP', 'OP_HASH160', p2pkh_pk.to_hash160(),
+                            'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+    # create the signature for redeeming the funds
+    sig = p2pkh_sk.sign_input(tx, 0, redeem_script)
+    # sign every input of the transaction
+    for txin in tx.inputs:
+        txin.script_sig = Script([sig, p2pkh_pk.to_hex(), redeem_script.to_hex()])
+
+    print('Raw signed transaction:', tx.serialize())
+    print('Transaction ID:', tx.get_txid())
+
+    # verify that the transaction is valid
+    ver = rpc_proxy.testmempoolaccept([tx.serialize()])
+    if ver[0]['allowed']:
+        # if the transaction is valid send the transaction to the blockchain
+        print('Transaction is valid.')
+        rpc_proxy.sendrawtransaction(tx.serialize())
+        print('{} Bitcoin sent to address {}'.format(total_unspent - fee, p2pkh_pub_addr))
+    else:
+        print('Transaction rejected. Reason:', ver[0]['reject-reason'])
